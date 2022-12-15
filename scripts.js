d80a0ca565d84d70a643ae4cad996e53b167dd39
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-15 11:09:59 -0300

    circle

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const screen = document.querySelector('canvas');
 const brush = screen.getContext('2d');
 
+ //retangulos
 brush.fillStyle = 'lightgrey';
 brush.fillRect(0, 0, 600, 400);
 
@@ -10,9 +11,18 @@ brush.fillRect(0, 0, 200, 400);
 brush.fillStyle = 'red';
 brush.fillRect(400, 0, 600, 400);
 
+//triangulo
 brush.fillStyle = 'blue';
 brush.beginPath();
 brush.moveTo(300, 200);
 brush.lineTo(200, 400);
 brush.lineTo(400,400);
 brush.fill();
+
+//circulo
+brush.fillStyle = 'yellow';
+brush.beginPath();
+brush.arc(300, 200, 50, 0, 2 * 3.14);
+brush.fill();
+
+
