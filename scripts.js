2fc8d847d6e09718615965124bdb734c75953ef2
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-12-15 11:05:11 -0300

    triangle

--- scripts.js
+++ scripts.js
@@ -3,3 +3,16 @@ const brush = screen.getContext('2d');
 
 brush.fillStyle = 'lightgrey';
 brush.fillRect(0, 0, 600, 400);
+
+brush.fillStyle = 'green';
+brush.fillRect(0, 0, 200, 400);
+
+brush.fillStyle = 'red';
+brush.fillRect(400, 0, 600, 400);
+
+brush.fillStyle = 'blue';
+brush.beginPath();
+brush.moveTo(300, 200);
+brush.lineTo(200, 400);
+brush.lineTo(400,400);
+brush.fill();
